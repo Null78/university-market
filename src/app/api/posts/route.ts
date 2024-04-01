ff6ebf26a486e557dc3382351dc618c5a5e359d4
Albaraa Hasan <albaraa.hassan@zid.sa>
2024-04-02 01:47:29 +0300

    initial posts routes

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -2,26 +2,42 @@ import prisma from "@/lib/prisma";
 
 // get all posts
 export async function GET(request: Request) {
-    // const { searchParams } = new URL(request.url) // get query params like ?search=hello
-    // const allPosts = await prisma.post.findMany({
-    //     where: {
-    //         deletedAt: null,
-    //     }
-    // });
+    const { searchParams } = new URL(request.url) // get query params like ?search=hello
+    const allPosts = await prisma.post.findMany({
+        where: {
+            deleted_at: null,
+        }
+    });
   
-    // return Response.json(allPosts);
+    return Response.json({
+        success: true,
+        message: "",
+        data: allPosts
+    });
 }
 
 // create a new post
 export async function POST(request: Request) {
-    // const body = await request.json();
-    //
-    // const newPost = await prisma.post.create({
-    //     data: {
-    //         title: body.title,
-    //         content: body.content,
-    //     }
-    // });
-  
-    // return Response.json(newPost);
+    const body = await request.json();
+    
+    const newPost = await prisma.post.create({
+        data: {
+            title: body.title,
+            description: body.description,
+            level: body.level,
+            location: body.location,
+            status: 'PENDING',
+            user: {
+                connect: {
+                    id: body.user_id
+                }
+            }
+        }
+    });
+
+    return Response.json({
+        success: true,
+        message: "",
+        data: newPost
+    });
 }
